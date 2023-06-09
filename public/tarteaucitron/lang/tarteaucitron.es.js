9dbb30cd5b4f21a62cb0283f8be22c1b35de87b8
fitodac <fitodac@gmail.com>
2023-06-09 08:01:10 -0300

    Public

---
+++ public/tarteaucitron/lang/tarteaucitron.es.js
@@ -0,0 +1,84 @@
+/*global tarteaucitron */
+tarteaucitron.lang = {
+    "middleBarHead": "Uso de cookies",
+    "adblock": "¡Hola! Este sitio web es transparente y te da la opción de activar los servicios de terceros.",
+    "adblock_call": "Por favor deshabilita tu AdBlocker para empezar a personalizar los servicios.",
+    "reload": "Actualizar esta página",
+    
+    "alertBigScroll": "Al continuar desplazándote,",
+    "alertBigClick": "Si continuas navegando por este sitio web,",
+    "alertBig": "estás permitiendo servicios terceros",
+    
+    "alertBigPrivacy": 'Utilizamos cookies propias y de terceros para finalidades analíticas, técnicas y de elaboración de perfiles; tratando datos necesarios para la elaboración de perfiles basados en tus hábitos de navegación. Pulsa el botón "ACEPTAR COOKIES" para la confirmación de su uso. Puedes cambiar la configuración de las cookies pulsando "MÁS INFORMACIÓN Y CONFIGURACIÓN".',
+    "alertSmall": "Gestionar servicios",
+    "personalize": "Personalizar",
+    "acceptAll": "OK, aceptar todas",
+    "close": "Cerrar",
+
+    "privacyUrl": "Política de privacidad",
+    
+    "all": "Ajustes para todos los servicios",
+
+    "info": "Protegiendo tu privacidad",
+    "disclaimer": "Uso de cookies en el Sitio Web",
+    "allow": "Permitir",
+    "deny": "Denegar",
+    "noCookie": "Este servicio no usa cookies.",
+    "useCookie": "Este servicio puede instalar",
+    "useCookieCurrent": "Este servicio ha instalado",
+    "useNoCookie": "Este servicio no ha instalado ninguna cookie.",
+    "more": "Leer más",
+    "source": "Ver sitio web oficial",
+    "credit": "Gestor de cookies realizado por tarteaucitron.js",
+
+    "toggleInfoBox": "Mostrar/ocultar información sobre almacenamiento de cookies",
+    "title": "Panel de gestión de cookies",
+    "cookieDetail": "Detalles de las cookies para",
+    "ourSite": "en nuestra web",
+    "modalWindow": "(ventana modal)",
+    "newWindow": "(ventana nueva)",
+    "allowAll": "Permitir todas las cookies",
+    "denyAll": "Denegar todas las cookies",
+
+    "icon": "Cookies",
+    
+    "fallback": "está deshabilitado.",
+    "allowed": "permitido",
+    "disallowed": "rechazado",
+
+    "ads": {
+        "title": "Red de publicidad",
+        "details": "Las redes publicitarias pueden generar ingresos mediante la venta de espacios publicitarios en el sitio."
+    },
+    "analytic": {
+        "title": "Medición de audiencia",
+        "details": "Los servicios de medición de audiencia se usan para generar estadísticas útiles para mejorar el sitio."
+    },
+    "social": {
+        "title": "Redes sociales",
+        "details": "Las redes sociales pueden aumentar la usabilidad del sitio web y ayudar a promoverlo a través de la contribución."
+    },
+    "video": {
+        "title": "Videos",
+        "details": "Los servicios para compartir videos ayudan a añadir contenido enriquecido en el sitio web y aumentar su visibilidad."
+    },
+    "comment": {
+        "title": "Comentarios",
+        "details": "El gestor de comentarios facilita la clasificación de comentarios y luchar contra spam."
+    },
+    "support": {
+        "title": "Soporte",
+        "details": "Los servicios de soporte te permiten contactar con el sitio web y ayudar a mejorarlo."
+    },
+    "api": {
+        "title": "APIs",
+        "details": "APIs se utilizan para cargar scripts: geolocalización, motor de búsqueda, traducciones, ..."
+    },
+    "other": {
+        "title": "Otro",
+        "details": "Servicios para mostrar contenido web."
+    },
+    
+    "mandatoryTitle": "Cookies obligatorias",
+    "mandatoryText": "Este sitio utiliza cookies necesarias para su correcto funcionamiento que no se pueden desactivar."
+};
